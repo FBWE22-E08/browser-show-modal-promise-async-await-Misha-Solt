5cbf4a820bef5daf1ab4f03f7de36b4c53f09d0b
Misha Solt <mikhail.us@gmail.com>
2023-01-30 16:50:34 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./css/style.css"; //importing css
+
+const modal = document.querySelector(".modal");
+const close = document.querySelector('.close');
+
+const Promise = async () =>{
+    setTimeout(() => {
+        modal.style.display = 'block';
+        close.style.cursor = 'pointer';
+    }, 2000);
+    close.onclick = () => {
+        modal.style.display = 'none';
+    }
+}
+
+Promise()
